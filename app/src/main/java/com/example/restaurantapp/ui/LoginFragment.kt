ff2f5a4ed81030aa96a42357280b1f2009b5610e
Matheus Del Nero <matheusdelnerovendas@gmail.com>
2023-05-02 15:01:30 -0300

    Login Screen

--- app/src/main/java/com/example/restaurantapp/ui/LoginFragment.kt
+++ app/src/main/java/com/example/restaurantapp/ui/LoginFragment.kt
@@ -1,60 +1,58 @@
 package com.example.restaurantapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.restaurantapp.MainActivity
 import com.example.restaurantapp.R
+import com.example.restaurantapp.databinding.FragmentLoginBinding
+import com.example.restaurantapp.ui.Home.HomeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var viewModel: HomeViewModel
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        viewModel = (activity as MainActivity).viewModel
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.singUp.setOnClickListener {
+            it.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            binding.singUp.setTextColor(resources.getColor(R.color.white,null))
+            binding.logIn.background = null
+            binding.singUpLayout.visibility = View.VISIBLE
+            binding.logInLayout.visibility = View.GONE
+            binding.logIn.setTextColor(resources.getColor(R.color.strongOrange,null))
+        }
+        binding.logIn.setOnClickListener {
+            binding.singUp.background = null
+            binding.singUp.setTextColor(resources.getColor(R.color.strongOrange,null))
+            binding.logIn.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            binding.singUpLayout.visibility = View.GONE
+            binding.logInLayout.visibility = View.VISIBLE
+            binding.logIn.setTextColor(resources.getColor(R.color.white,null))
+        }
+
+
     }
+
 }
